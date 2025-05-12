f1af53568474a9530bb6d53bccc22a9d9703c4fe
Arturo Bautista de Jesus <abautistad@megacable.com.mx>
2025-05-12 11:43:54 -0600

    Actividad 1

---
+++ Modulo_3_Dot_Net/01_sesion/proyecto/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempts = 3;
+            Dictionary<string, string> users = new Dictionary<string, string>()
+            {
+                { "admin", "p4$w0rd" },
+                { "user", "k3yp4ss" }
+            };
+
+            while(attempts > 0)
+            {
+                Console.WriteLine("Inicia Sesion.\n");
+
+                Console.Write("Usuario: ");
+                string? username = Console.ReadLine();
+                Console.Write("Contraseña: ");
+                string? password = Console.ReadLine();
+
+                var isValidUser = users.FirstOrDefault(x => x.Key == username && x.Value == password);
+                if (isValidUser.Key != null)
+                {
+                    Console.WriteLine("\nBienvenido " + username);
+                    break;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine("\nUsuario o contraseña incorrectos. Te quedan " + attempts + " intentos.");
+                    Console.WriteLine("Presiona cualquier tecla para continuar...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+
+            Console.WriteLine("Presiona cualquier tecla para finalizar...");
+            Console.ReadKey();
+
+        }
+    }
+}
