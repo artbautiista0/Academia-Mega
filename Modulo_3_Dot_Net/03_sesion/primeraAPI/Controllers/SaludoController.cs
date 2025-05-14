ca44f31ba3fae0142d52e11237b84b654dc77b9b
Arturo Bautista de Jesus <abautistad@megacable.com.mx>
2025-05-14 10:33:03 -0600

    Actividad 3

---
+++ Modulo_3_Dot_Net/03_sesion/primeraAPI/Controllers/SaludoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using primeraAPI.Models;
+
+namespace primeraAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SaludoController : ControllerBase
+    {
+        
+        [HttpGet]
+        [Route("saludo")]
+        public IActionResult GetSaludo()
+        {
+            try
+            {
+                Saludo saludo = new();
+                string result = saludo.GetSaludo();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = result });
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Error interno de servidor " + e });
+            }
+
+        }
+
+        [HttpGet]
+        [Route("saludoParametro/{nombre}")]
+        public IActionResult GetSaludoParametro(string nombre)
+        {
+            try
+            {
+                Saludo saludo = new Saludo(nombre, "Hola");
+                string result = saludo.GetSaludo();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = result });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Error interno de servidor " + e });
+            }
+        }
+    }
+}
---
+++ Modulo_3_Dot_Net/03_sesion/primeraAPI/Models/Saludo.cs
@@ -0,0 +1,23 @@
+﻿namespace primeraAPI.Models
+{
+    public class Saludo
+    {
+        public string? Nombre { get; set; }
+        public string? Mensaje { get; set; }
+        public Saludo(string nombre, string mensaje)
+        {
+            Nombre = nombre;
+            Mensaje = mensaje;
+        }
+        public Saludo()
+        {
+            Nombre = "Arturo";
+            Mensaje = "Hola";
+        }
+
+        public string GetSaludo()
+        {
+            return Mensaje + " " + Nombre;
+        }
+    }
+}
